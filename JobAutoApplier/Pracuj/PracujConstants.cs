5913126b41aa615d666aa75677e2ee19546947b7
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2026-01-04 22:23:02 +0100

    Add credentials loading to PracujConstants

--- JobAutoApplier/Pracuj/PracujConstants.cs
+++ JobAutoApplier/Pracuj/PracujConstants.cs
@@ -12,15 +12,18 @@ public static class PracujConstants
     public const string ScrollDownExpression = "window.scrollTo(0, document.body.scrollHeight)";
     public const int CookiePopupTimeout = 5000;
     
-    // Login
     public const string LoginPageUrl = "https://login.pracuj.pl/";
-    
     public const string LoginInputSelector = "#email";
     public const string PasswordInputSelector = "#password";
-    
-    public const string Login = "";
-    public const string Password = "";
-    
     public const string LoginNextButtonSelector = "text=Dalej";
     public const string LoginButtonSelector = "text=Zaloguj";
+    
+    public static string Login { get; private set; } = string.Empty;
+    public static string Password { get; private set; } = string.Empty;
+    
+    public static void LoadCredentials(PortalCredentials credentials)
+    {
+        Login = credentials.Login;
+        Password = credentials.Password;
+    }
 }
